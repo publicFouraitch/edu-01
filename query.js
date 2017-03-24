cbe22dcdf5df258147fe849645b4507699665a0b
publicFouraitch <ashot.mnatsakanyan@4h.am>
2017-03-24 11:09:18 +0400

    console.log( module.exports.english["A1.2-6"]); senc tpum enq konkret mi sharq, hima uzum enq dimenq indexov, vor karoxananq dnell funkciayi mej

--- query.js
+++ query.js
@@ -1,9 +1,9 @@
- var data = require('data.js');
+ var data = require('./data');
 
 const co = require("co");
 
 function  doRequest(lang, section, coveragePercent, words, minimumShouldPercent) {
-    
+
     const filter = [
         {term: {lang: lang}},
         {term: {sectionPercent90: section}},
@@ -23,7 +23,7 @@ function  doRequest(lang, section, coveragePercent, words, minimumShouldPercent)
             });
         });
         //boolQuery.minimum_should_match = Math.ceil(words.length / 2);
-        if (minimumShouldPercent!=0)
+        if (!minimumShouldPercent)
         {
             boolQuery.minimum_should_match = Math.ceil(words.length * minimumShouldPercent / 100);
             console.log(boolQuery.minimum_should_match);
@@ -36,19 +36,19 @@ function  doRequest(lang, section, coveragePercent, words, minimumShouldPercent)
     //     delete boolQuery.minimum_should_match;
     // }
 
-/*
-    console.log(JSON.stringify({
-        index: "indexname",
-        type: "paragraphs",
-        from: 0,
-        size: 20,
-        body: {
-            query: {
-                bool: boolQuery,
-            }
-        },
-    }, null, 2));
-*/
+    /*
+     console.log(JSON.stringify({
+     index: "indexname",
+     type: "paragraphs",
+     from: 0,
+     size: 20,
+     body: {
+     query: {
+     bool: boolQuery,
+     }
+     },
+     }, null, 2));
+     */
     // const result = yield elasticsearch.count({
     //     index: "indexname",
     //     type: "paragraphs",
@@ -66,15 +66,7 @@ function  doRequest(lang, section, coveragePercent, words, minimumShouldPercent)
 
 
 
-
 //return Math.floor(Math.random() * 100);
 
-doRequest("german", "B1.1-1", 45, ["hello", "world"],10);
-
-
-
-
-
-
-
-
+// doRequest("german", "B1.1-1", 45, ["hello", "world"],10);
+doRequest("german", "B1.1-1", 6795, ["hello", "world"],10);
