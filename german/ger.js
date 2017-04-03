2a701fb905080398ba143d38ee661dd056c2543c
publicFouraitch <ashot.mnatsakanyan@4h.am>
2017-04-03 12:37:00 +0400

    export.json faylum arden kan duplicate toxeri articl@ word@ u wclass@ arrayov

--- german/ger.js
+++ german/ger.js
@@ -1,17 +1,46 @@
 
 var data = require("./convertcsv.json");
-var fs = require('fs')
+var fs = require('fs');
+
+// var csv = require('fast-csv');
+//
+// fs.createReadStream('german.csv')
+//     .pipe(csv())
+//     .on('data', function(data){
+//         console.log(data);
+//     })
+//     .on('end', function(data){
+//         console.log('Read finished');
+//     });
 
 
 var articleArr = data.article;
 var wordArr = data.word;
 var wclassArr = data.wclass;
-var dup
-for (var i = 0; i < articleArr.length - 1; ++i)
-{
-    for (var j = i; j  < articleArr.length - 1; ++j)
-    {
+var dupArr = {"article":[], "word":[], "wclass": []};
 
-    }
+    for (var i = 0; i < articleArr.length - 1; ++i)
+    {
+        for (var j = i; j  < articleArr.length - 1; ++j)
+        {
+            if (articleArr[i] == articleArr[j + 1] && wordArr[i] == wordArr[j + 1] && wclassArr[i] == wclassArr[j + 1]) {
+                dupArr.article.push(articleArr[i]);
+                dupArr.word.push(wordArr[i]);
+                dupArr.wclass.push(wclassArr[i]);
+            }
+        }
 
 }
+
+const jsonString = JSON.stringify(dupArr);
+
+fs.writeFile('./german/export.json', jsonString, 'utf-8', function(err)
+  {
+    if(!err)
+        ;
+     else
+        console.log("Error!");
+  });
+
+
+
